5c62a8e6f7cb82346f2f47bb971dcc512112a0cf
R.K.Lakshan <lakshan2725@gmail.com>
2025-09-03 01:39:44 +0530

    [SL-B-001] | userModule modified

---
+++ src/modules/userModule.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["user", "driver"],
+  },
+});
+
+const User = mongoose.model.user || mongoose.model("User", userSchema);
+
+export default User;
