28446514286fb16429a902bcc802eee5b02680ff
Sakuna Sanka <sakunasanka@gmail.com>
2025-09-09 23:15:55 +0530

    Fixed schema errors

--- src/server.js
+++ src/server.js
@@ -17,5 +17,5 @@ app.get("/", (req, res) => {
   res.json({ message: "Backend is running 🚀" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5999;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
